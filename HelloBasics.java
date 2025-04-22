075215355c9ba502aad724441e8f7e1be830c830
phosugabby <phosuantwi@gmail.com>
2025-04-22 14:33:03 +0000

    love

---
+++ HelloBasics.java
@@ -0,0 +1,8 @@
+public class HelloBasics {
+    public static void main(String[] args) {
+        int number = 5;
+        int number2 = 7;
+        int add = number + number2;
+        System.out.println("Add:" + add);
+    }
+}
